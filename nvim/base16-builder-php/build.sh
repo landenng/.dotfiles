5e2d62098c282943acd4d5227d02027adbefa6dd
Landen Nguyen <landenttvnguyen@gmail.com>
2023-08-31 13:19:07 -0500

    Remove directory from cache

--- nvim/base16-builder-php/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cat scheme.yaml | php base16-builder.php --template vim.mustache > ../schemes/colors/catppuccin.vim
-cat scheme.yaml | php base16-builder.php --template sh.mustache > ../schemes/shell/catppuccin.sh
-source ../schemes/shell/catppuccin.sh

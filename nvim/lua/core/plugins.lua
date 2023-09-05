4d7e8366c42a04b492da1eceb3d8d2c011e12e18
Landen Nguyen <landenttvnguyen@gmail.com>
2023-09-05 16:21:05 -0500

    Replace 'packer.nvim' with 'lazy.nvim'
    Packer is no longer being maintained as of August 2023 which is
    why it is being replaced with lazy.nvim.

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -1,59 +1,52 @@
--- This is a bootstrap file is used to check if packer is installed on a machine.
--- If it is not, it will automatically install packer and any plugins
--- specified below.
--- Found at https://github.com/wbthomason/packer.nvim#bootstrapping
-
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+-- This command bootstraps lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
-
-local packer_bootstrap = ensure_packer()
+vim.opt.rtp:prepend(lazypath)
 
 -- Add plugins here
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use { "catppuccin/nvim", as = "catppuccin" }
-  use 'nvim-tree/nvim-web-devicons'
-  use 'nvim-lualine/lualine.nvim'
-  use 'nvim-treesitter/nvim-treesitter'
+local plugins = {
+  { "catppuccin/nvim", as = "catppuccin" },
+  'nvim-tree/nvim-web-devicons',
+  'nvim-lualine/lualine.nvim',
+  'nvim-treesitter/nvim-treesitter',
 
   -- autocompletion and snippet integration
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'L3MON4D3/LuaSnip'
+  'hrsh7th/nvim-cmp',
+  'hrsh7th/cmp-nvim-lsp',
+  'L3MON4D3/LuaSnip',
 
   -- vs-code like snippets
-  use 'saadparwaiz1/cmp_luasnip'
-  use 'rafamadriz/friendly-snippets'
+  'saadparwaiz1/cmp_luasnip',
+  'rafamadriz/friendly-snippets',
 
   -- fuzzy finder integration and file tree
-  use {
+  {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.0',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
-  use {
+    dependencies = { {'nvim-lua/plenary.nvim'} }
+  },
+  {
     "nvim-telescope/telescope-file-browser.nvim",
-    requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
-  }
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+  },
 
   -- lsp integration
-  use {
+  {
     'williamboman/mason.nvim',
     'williamboman/mason-lspconfig.nvim',
     'neovim/nvim-lspconfig',
   }
+}
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
+local opts = {}
+
+require("lazy").setup(plugins, opts)
